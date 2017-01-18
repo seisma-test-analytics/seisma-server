d1aecfc9211c66b3cb03bd4da1c1fb8380844f7b
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2017-01-18 16:30:31 +0300

    fix get builds for dashboard

--- seisma/api/resourses/v1/dashboard.py
+++ seisma/api/resourses/v1/dashboard.py
@@ -69,6 +69,7 @@ def get_last_failed_builds():
     """
     query = db.Build.query.filter(
         db.Build.was_success == False,
+        db.Build.is_running == False,
         db.Build.date >= _get_date_from(),
     )
     query = query.order_by(desc(db.Build.date))
